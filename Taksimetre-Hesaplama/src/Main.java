a5a4ad989c8f95866abea494fea3ddbfcbbc9b57
rbatuhan <bthnercn99@gmail.com>
2023-12-17 19:21:46 +0300

    Taksimetre Hesaplayan Program

---
+++ Taksimetre-Hesaplama/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Değişkenleri tanımlama.
+        int km;
+        double perKm = 2.20, total, startPrice = 10;
+
+        // Kullanıcıdan veri alma.
+        Scanner input = new Scanner(System.in);
+        System.out.print("Mesafeyi KM cinsinden giriniz : ");
+        km = input.nextInt();
+
+        total = (km * perKm);
+        total += startPrice;
+
+        total = (total < 20) ? 20 : total;
+        System.out.println("Toplam Tutar : " + total);
+
+    }
+}
