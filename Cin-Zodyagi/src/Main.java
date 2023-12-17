eb1ca3d7b38747e4f66fb488aae64a026c39247a
rbatuhan <bthnercn99@gmail.com>
2023-12-17 19:36:48 +0300

    Çin Zodyağı Hesaplama

---
+++ Cin-Zodyagi/src/Main.java
@@ -0,0 +1,57 @@
+import java.lang.invoke.SwitchPoint;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Değişkenleri atayalım
+        int dogumYili;
+
+        //Değişken verilerini alalım.
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Doğum yılınızı giriniz : ");
+        dogumYili = input.nextInt();
+
+        switch ( dogumYili % 12) {
+            case 0:
+                System.out.println("Çin Zodyağı Burcunuz : Maymun");
+                break;
+            case 1:
+                System.out.println("Çin Zodyağı Burcunuz : Horoz");
+                break;
+            case 2:
+                System.out.println("Çin Zodyağı Burcunuz : Köpek");
+                break;
+            case 3:
+                System.out.println("Çin Zodyağı Burcunuz : Domuz");
+                break;
+            case 4:
+                System.out.println("Çin Zodyağı Burcunuz : Fare");
+                break;
+            case 5:
+                System.out.println("Çin Zodyağı Burcunuz : Öküz");
+                break;
+            case 6:
+                System.out.println("Çin Zodyağı Burcunuz : Kaplan");
+                break;
+            case 7:
+                System.out.println("Çin Zodyağı Burcunuz : Tavşan");
+                break;
+            case 8:
+                System.out.println("Çin Zodyağı Burcunuz : Ejderha");
+                break;
+            case 9:
+                System.out.println("Çin Zodyağı Burcunuz : Yılan");
+                break;
+            case 10:
+                System.out.println("Çin Zodyağı Burcunuz : At");
+                break;
+            case 11:
+                System.out.println("Çin Zodyağı Burcunuz : Koyun");
+                break;
+            default:
+                System.out.println("Hatalı Değer Girdiniz!");
+        }
+    }
+}
